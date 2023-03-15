5ef7e44223aa4429559fc63ca0b887c24725a0a6
eduim <eduardo.im92@gmail.com>
2023-03-15 13:43:52 +0100

    api(client): added server api

---
+++ apps/client/utils/server-api.js
@@ -0,0 +1,30 @@
+const BASE_URL = "http://localhost:8080";
+
+const serverAPI = {
+  async getMetrics() {
+    const response = await fetch(`${BASE_URL}/metrics`);
+    const data = await response.json();
+    return data;
+  },
+
+  async pushMetrics(name) {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+      }),
+    };
+    const response = await fetch(`${BASE_URL}/metrics`, options);
+    const data = await response.json();
+    return data;
+  },
+
+  async getMetric(id) {
+    const response = await fetch(`${BASE_URL}/metrics/${id}`);
+    const data = await response.json();
+    return data;
+  },
+};
