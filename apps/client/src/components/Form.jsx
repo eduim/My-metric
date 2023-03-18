79d17f609a549c17c478e878d1bc1605b4e09324
eduim <eduardo.im92@gmail.com>
2023-03-18 19:13:30 +0100

    fixed(form): click to submit added

--- apps/client/src/components/Form.jsx
+++ apps/client/src/components/Form.jsx
@@ -36,6 +36,7 @@ export default function Form({ onSetMetrics, selected }) {
         <button
           type="button"
           className="inline-flex items-center rounded-md bg-fuchsia-300 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-fuchsia-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-fuchsia-500"
+          onClick={handleSubmit}
         >
           +
         </button>
